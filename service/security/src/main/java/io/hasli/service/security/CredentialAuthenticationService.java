82805ac233ce2ef1a67860330d79eae5916f7eb8
Josiah Haswell <josiah.haswell@gmail.com>
2016-10-11 12:00:52 -0600

    Initial Arquillian JUnit test

---
+++ service/security/src/main/java/io/hasli/service/security/CredentialAuthenticationService.java
@@ -0,0 +1,7 @@
+package io.hasli.service.security;
+
+/**
+ * Created by haswell on 10/11/16.
+ */
+public class CredentialAuthenticationService {
+}
---
+++ service/security/src/test/java/io/io/hasli/service/security/CredentialAuthenticationServiceTest.java
@@ -0,0 +1,42 @@
+package io.io.hasli.service.security;
+
+import javax.inject.Inject;
+
+import io.hasli.service.security.CredentialAuthenticationService;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ * Created by haswell on 10/11/16.
+ */
+
+@RunWith(Arquillian.class)
+public class CredentialAuthenticationServiceTest {
+
+    @Inject
+    CredentialAuthenticationService credentialService;
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(CredentialAuthenticationService.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+
+    @Test
+    public void ensureAuthenticatingWithUsernameAndPasswordProducesToken() {
+        assertThat(credentialService, is(not(nullValue())));
+
+    }
+
+
+}
