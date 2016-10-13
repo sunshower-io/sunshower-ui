8907c58d0063f8aa52dcc809903e207aa9cc1800
Josiah Haswell <josiah.haswell@gmail.com>
2016-10-12 22:58:44 -0600

    Adding authentication service

--- api/core/src/main/java/io/hasli/model/core/auth/User.java
+++ api/core/src/main/java/io/hasli/model/core/auth/User.java
@@ -28,6 +28,29 @@ public class User extends AbstractEntity<UUID> {
 
     }
 
+    public Details getDetails() {
+        return details;
+    }
+
+    public void setDetails(Details details) {
+        this.details = details;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public boolean equals(Object obj) {
---
+++ api/security/src/main/java/io/hasli/core/security/AuthenticationService.java
@@ -0,0 +1,7 @@
+package io.hasli.core.security;
+
+/**
+ * Created by haswell on 10/12/16.
+ */
+public interface AuthenticationService {
+}
