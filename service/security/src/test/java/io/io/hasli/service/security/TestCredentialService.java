47ad48e7c8d7b301557482e53182ac8d5e86da2e
Josiah Haswell <josiah.haswell@gmail.com>
2016-10-17 14:13:08 -0600

    Adding HikariCP module

---
+++ service/security/src/test/java/io/io/hasli/service/security/TestCredentialService.java
@@ -0,0 +1,24 @@
+package io.io.hasli.service.security;
+
+import io.hasli.core.security.CredentialService;
+import io.hasli.model.core.auth.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created by haswell on 10/17/16.
+ */
+public class TestCredentialService implements CredentialService {
+    @Override
+    public User authenticate() {
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User u = new User();
+        u.setPassword("frap");
+        u.setUsername(username);
+        return u;
+    }
+}
--- service/security/src/test/java/io/io/hasli/service/security/TestSecurityConfiguration.java
+++ service/security/src/test/java/io/io/hasli/service/security/TestSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package io.io.hasli.service.security;
 
+import io.hasli.core.security.CredentialService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,11 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class TestSecurityConfiguration {
+
+    @Bean
+    public CredentialService credentialService() {
+        return new TestCredentialService();
+    }
 
     @Bean
     public TestSecureService testService() {
