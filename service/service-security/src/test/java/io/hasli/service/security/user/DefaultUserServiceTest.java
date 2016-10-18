a4160ee95abf7278067f925a88726bf9c0fdf7c0
Josiah Haswell <josiah.haswell@gmail.com>
2016-10-18 15:29:43 -0600

    Renaming projects to avoid conflicts

---
+++ service/service-security/src/test/java/io/hasli/service/security/user/DefaultUserServiceTest.java
@@ -0,0 +1,13 @@
+package io.hasli.service.security.user;
+
+import org.junit.Ignore;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by haswell on 10/18/16.
+ */
+@Ignore
+public class DefaultUserServiceTest {
+
+}
