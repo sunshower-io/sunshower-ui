33503ea507ad955cd68d7b66e7a71c4323676440
Dustin Lish <dlish27@gmail.com>
2016-10-25 22:40:57 -0600

    adding more routes for current navbar items

--- web/web-ui/src/app.ts
+++ web/web-ui/src/app.ts
@@ -10,25 +10,54 @@ export class App {
         config.map([
             {
                 route: ['', 'home'],
+                redirect: 'dashboard',
                 name: 'home',
                 moduleId: 'home/home',
-                nav: true,
+                nav: false,
                 title: 'home'
             }, {
-                route: 'login',
-                name: 'login',
-                moduleId: 'login/login',
+                route: 'dashboard',
+                name: 'Dashboard',
+                moduleId: 'dashboard/dashboard',
                 nav: true,
-                title: 'login'
+                title: 'Dashboard'
             }, {
-
-                route: 'signup',
-                name: 'signup',
-                moduleId: 'signup/signup',
+                route: 'applications',
+                name: 'Applications',
+                moduleId: 'applications/applications',
                 nav: true,
-                title: 'signup'
+                title: 'Applications'
+            }, {
+                route: 'infrastructure',
+                name: 'Infrastructure',
+                moduleId: 'infrastructure/infrastructure',
+                nav: true,
+                title: 'Infrastructure'
+            }, {
+                route: 'storage',
+                name: 'Storage',
+                moduleId: 'storage/storage',
+                nav: true,
+                title: 'Storage'
+            }, {
+                route: 'manage',
+                name: 'Manage',
+                moduleId: 'manage/manage',
+                nav: true,
+                title: 'Manage'
+            }, {
+                route: 'reports',
+                name: 'Reports',
+                moduleId: 'reports/reports',
+                nav: true,
+                title: 'Reports'
+            }, {
+                route: 'logs',
+                name: 'Logs',
+                moduleId: 'logs/logs',
+                nav: true,
+                title: 'Logs'
             }
-
         ]);
 
         this.router = router;
