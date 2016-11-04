3c499730e3cb74e2e3510f793810234a9999f2b8
Josiah Haswell <josiah.haswell@gmail.com>
2016-11-03 19:26:11 -0600

    Adding subrouter for infrastructure

--- web/web-ui/src/main/infrastructure/infrastructure.ts
+++ web/web-ui/src/main/infrastructure/infrastructure.ts
@@ -1,4 +1,31 @@
 
-export class Infrastructure {
 
+import {
+    Router,
+    RouterConfiguration
+} from "aurelia-router";
+
+
+export class Infrastructure {
+    public router:Router;
+
+    public configureRouter(config:RouterConfiguration, router:Router) {
+        config.map([
+            {
+                route: ['', 'credentials'],
+                name: 'credentials',
+                moduleId: './credentials/credentials',
+                nav: true,
+                title: 'credentials'
+            },
+            {
+                route: 'projects',
+                name: 'projects',
+                moduleId: './projects/projects',
+                nav: true,
+                title: 'projects'
+            }
+        ]);
+        this.router = router;
+    }
 }
