e08ec5bcae4cdc359bd5d2daa6fd7900f40fa81d
Dustin Lish <dlish27@gmail.com>
2016-11-05 00:13:38 -0600

    adding initial framing ideas for dashboard and app

--- web/web-ui/src/main/applications/applications.ts
+++ web/web-ui/src/main/applications/applications.ts
@@ -1,4 +1,24 @@
+import 'jquery';
 
 export class Applications {
+
+    attached() {
+        $('.ui.accordion')
+            .accordion();
+
+        $('.tabular.menu .tab.item')
+            .tab();
+
+        $('.plus--icon').click(function(){
+            console.log("plus icon clicked");
+            // $(this)
+            //     .transition('fade');
+            // $("img.plus--icon").toggleClass("transparent");
+            // $("img.minus--icon").toggleClass("transparent");
+        });
+
+
+
+    }
 
 }
