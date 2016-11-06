16e817876e58f0636d2493f10230e3ee3ccaade2
Josiah Haswell <josiah.haswell@gmail.com>
2016-11-06 16:22:41 -0700

    Saving promise

--- web/web-ui/typings/index.d.ts
+++ web/web-ui/typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/selenium-webdriver/index.d.ts" />
