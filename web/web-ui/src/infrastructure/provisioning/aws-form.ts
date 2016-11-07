8fe2228616023a6ac55f2a64479c7914f2e95399
Dustin Lish <dlish27@gmail.com>
2016-11-06 22:36:13 -0700

    initial infrastructure framing

--- web/web-ui/src/infrastructure/provisioning/aws-form.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by dustinlish on 11/3/16.
- */
-
-export class AwsForm {
-
-}
--- web/web-ui/src/main/infrastructure/infrastructure.ts
+++ web/web-ui/src/main/infrastructure/infrastructure.ts
@@ -5,27 +5,43 @@ import {
     RouterConfiguration
 } from "aurelia-router";
 
+import {Form} from 'form/form'
+import {inject} from "aurelia-dependency-injection";
 
+@inject(Form)
 export class Infrastructure {
-    public router:Router;
 
-    public configureRouter(config:RouterConfiguration, router:Router) {
-        config.map([
-            {
-                route: ['', 'credentials'],
-                name: 'credentials',
-                moduleId: './credentials/credentials',
-                nav: true,
-                title: 'credentials'
-            },
-            {
-                route: 'projects',
-                name: 'projects',
-                moduleId: './projects/projects',
-                nav: true,
-                title: 'projects'
-            }
-        ]);
-        this.router = router;
+    constructor(private form: Form) {
+        this.form = form;
     }
+
+    attached() {
+        $('.ui.selection.dropdown').dropdown();
+    }
+
+    create() {
+        console.log('create button clicked');
+        this.form.show();
+    }
+    // public router:Router;
+    //
+    // public configureRouter(config:RouterConfiguration, router:Router) {
+    //     config.map([
+    //         // {
+    //         //     route: ['', 'credentials'],
+    //         //     name: 'credentials',
+    //         //     moduleId: './credentials/credentials',
+    //         //     nav: true,
+    //         //     title: 'credentials'
+    //         // },
+    //         // {
+    //         //     route: 'projects',
+    //         //     name: 'projects',
+    //         //     moduleId: './projects/projects',
+    //         //     nav: true,
+    //         //     title: 'projects'
+    //         // }
+    //     ]);
+    //     this.router = router;
+    // }
 }
