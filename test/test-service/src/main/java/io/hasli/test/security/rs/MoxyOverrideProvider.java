e820b26a6849bc083f00f98e16ebff2d05d2b007
Josiah Haswell <josiah.haswell@gmail.com>
2016-11-07 14:41:26 -0700

    Fix for serializing UUIDs as responses
    Conflicts:
    	bom/imported/pom.xml
    	service/service-hal/src/test/java/io/hasli/hal/core/node/DefaultNodeConfigurationServiceTest.java

---
+++ test/test-service/src/main/java/io/hasli/test/security/rs/MoxyOverrideProvider.java
@@ -0,0 +1,25 @@
+package io.hasli.test.security.rs;
+
+import io.hasli.barometer.jaxrs.ProviderOverride;
+import io.hasli.common.rs.DefaultConversionManager;
+import org.eclipse.persistence.internal.helper.ConversionManager;
+import org.eclipse.persistence.jaxb.rs.MOXyJsonProvider;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Created by haswell on 11/7/16.
+ */
+@Provider
+@ProviderOverride
+@Produces({MediaType.APPLICATION_JSON, MediaType.WILDCARD, "application/x-javascript"})
+@Consumes({MediaType.APPLICATION_JSON, MediaType.WILDCARD})
+public class MoxyOverrideProvider extends MOXyJsonProvider {
+
+    static {
+        ConversionManager.setDefaultManager(DefaultConversionManager.getInstance());
+    }
+}
