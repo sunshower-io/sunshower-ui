c985954de2fc61a61c7a0395b7039ac64fe4c834
Dustin Lish <dlish27@gmail.com>
2016-11-07 16:06:00 -0700

    adding initial applications routes

--- web/web-ui/src/main/applications/applications.ts
+++ web/web-ui/src/main/applications/applications.ts
@@ -1,6 +1,50 @@
 import 'jquery';
+import {RouterConfiguration} from "aurelia-router";
+import {Router} from "aurelia-router";
 
 export class Applications {
+
+    public router: Router;
+
+    public configureRouter(config: RouterConfiguration, router: Router) {
+        config.title = 'Applications';
+        config.map([
+            {
+                route: 'myapps',
+                name: 'myapps',
+                moduleId: 'main/applications/apps/my-apps',
+                nav: true,
+                title: 'My Applications'
+            }, {
+                route: ['', 'instances'],
+                name: 'instances',
+                moduleId: 'main/applications/instances/instances',
+                nav: true,
+                title: 'Instances'
+            }, {
+                route: 'deploy',
+                name: 'deploy',
+                moduleId: 'main/applications/deploy/deploy',
+                nav: true,
+                title: 'Deploy'
+            }, {
+                route: 'Explore',
+                name: 'explore',
+                moduleId: 'main/applications/explore/explore',
+                nav: true,
+                title: 'Explore'
+            }, {
+                route: 'library',
+                name: 'library',
+                moduleId: 'main/applications/library/library',
+                nav: true,
+                title: 'Library'
+
+            }
+        ]);
+
+        this.router = router;
+    }
 
     attached() {
         $('.ui.accordion').accordion();
