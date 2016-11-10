1def3d72399384914d265c89ef7ced127c7d3e97
Josiah Haswell <josiah.haswell@gmail.com>
2016-11-10 14:58:07 -0700

    Adding some stuff

--- web/web-ui/typings/index.d.ts
+++ web/web-ui/typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/dropzone/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/jquery.pnotify/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
