d81979889c9d8e5d8db22bba6b99d1964446af1e
Dustin Lish <dlish27@gmail.com>
2016-11-14 16:48:21 -0700

    adding intial build layout with side menus

--- web/web-ui/src/main/workspace/builder/builder.ts
+++ web/web-ui/src/main/workspace/builder/builder.ts
@@ -3,4 +3,34 @@
  */
 export class Builder {
 
+
+    attached() {
+        $('.ui.dropdown')
+            .dropdown();
+
+        $('.side.menus .ui.sidebar')
+            .sidebar({
+                dimPage: false,
+                transition: 'overlay',
+                exclusive: false,
+                closable: false
+            })
+            .sidebar('toggle');
+
+
+        // $('body').css('overflow', 'hidden');
+
+    }
+
+    detached() {
+        // $('body').css('overflow', 'auto');
+    }
+
+    toggle(menu) {
+        console.log("toggling: " + menu);
+        $(`.${menu}--menu`)
+            .sidebar('toggle')
+        ;
+    }
+
 }
