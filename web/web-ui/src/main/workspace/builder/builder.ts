57fc3ccd98c8839225d7b5f273e75f5dd3b65180
Josiah Haswell <josiah.haswell@gmail.com>
2016-11-28 17:32:01 -0700

    Fixing grid

--- web/web-ui/src/main/workspace/builder/builder.ts
+++ web/web-ui/src/main/workspace/builder/builder.ts
@@ -1,230 +1,69 @@
 /**
  * Created by dustinlish on 11/9/16.
  */
+import {
+    mxGraph,
+    mxClient,
+    mxUtils,
+    mxEvent,
+    mxRubberband,
+    mxGraphModel
+} from 'mxgraph';
 
-import * as gridGuide from 'cytoscape-grid-guide'
-import {inject, bindable} from 'aurelia-framework';
-import {ImageDescriptor} from '../../../model/hal/image'
+import {PLATFORM} from 'aurelia-pal';
 import {HttpClient} from "aurelia-fetch-client";
-import * as ocanvas from 'ocanvas';
+import {inject, bindable} from 'aurelia-framework';
+import {Grid} from "./grid";
 
-
-import * as nodeResize from 'cytoscape-node-resize';
-import * as cytoscape from 'cytoscape';
-import * as edgehandles from 'cytoscape-edgehandles';
 
 @inject(HttpClient)
 export class Builder {
 
-    private leftVisible: boolean = true;
-    private rightVisible: boolean = true;
-    private expanded: number = 2;
-    private states: {[key: number]: string} = {};
+    graph: mxGraph;
 
+    grid: Grid;
+    container: HTMLElement;
 
-    @bindable
-    private graphInstance:any;
-    private graph: HTMLElement;
+    resizeHandler = () => this.resized();
 
-    private leftSidebar: HTMLElement;
-
-    private rightSidebar: HTMLElement;
-
-
-    constructor(private client:HttpClient) {
-        this.states[2] = "ten";
-        this.states[0] = "sixteen";
-        this.states[1] = "thirteen";
-    }
-
-
-    public attached(): void {
-        this.configureWorkspace();
-        this.configureSidebars();
-    }
-
-    private configureWorkspace(): void {
-        let cy = cytoscape({
-            container: this.graph,
-            boxSelectionEnabled:true,
-            ready: (e) => {
-                gridGuide(cytoscape);
-                nodeResize(cytoscape, $, ocanvas);
-                edgehandles(cytoscape);
-            },
-
-            style: (<any>cytoscape).stylesheet()
-                .selector('edge')
-                .css({
-                    'curve-style': 'bezier',
-                    'target-arrow-shape': 'triangle',
-                    'target-arrow-color': '#ccc',
-                    'line-color': '#ccc',
-                    'width': 1
-                })
-                .selector('node[icon]').style({
-                    "background-width":24,
-                    "background-height":24,
-                    "background-color" : "white",
-                    "background-position-x": "8px",
-                    "border-color":"#a0a0a0",
-                    "border-width": "1px",
-                    "border-style": "solid",
-                    'shadow-blur': 10,
-                    'shadow-color': '#000000',
-                    'shadow-offset-x': 0,
-                    'shadow-offset-y': 0,
-                    'shadow-opacity': 0.9,
-                    "text-valign": "center",
-                    "text-halign": "center",
-                    'backgroundImage': 'data(icon)',
-                    "font-size": "1em",
-                    "font-family": "Open Sans",
-                    shape: 'rectangle',
-                    width:"140",
-                    height:"42",
-                    label: 'data(label)'
-                })
-                .selector('node:selected')
-                .style({
-                    "font-weight": "bold",
-                    "border-style": "dashed",
-                    "background-blacken": 0.1,
-                    "border-opacity": 1,
-                }),
-            elements: [],
-        });
-
-
-        this.graphInstance = cy;
-        (<any>window).cy = cy;
-        (<any>window).cytoscape = cy;
-
-        cy.nodeResize({
-            boundingRectangleLineColor: '#828282',
-            inactiveGrappleStroke: '0',
-            isNoResizeMode: (node) => {
-                return !node.data('resizable');
-            }
-        });
-
-        cy.edgehandles();
-
-        cy.gridGuide({
-            snapToGrid:true,
-            discreteDrag:true,
-            guidelines:true,
-            panGrid:true
-        });
-    }
-
-    toggleLeft() {
-        this.leftVisible = this.toggle(
-            this.leftSidebar,
-            this.leftVisible,
-            'right'
-        );
-    };
-
-    onItemDropped(e:Event) :void {
-        console.log(e);
-    }
-
-    onImageAdded(e:Event) : void {
-        let value = (<any>e).detail.value;
-
-        this.client.fetch(`docker/images/${value}`)
-            .then(r => r.json())
-            .then(r => this.add(r, e));
-    }
-
-    add(imageDescriptor: ImageDescriptor, e:any) {
-        console.log(imageDescriptor.logo_url.large)
-
-        let rawPosition = e.detail.location,
-            graphPosition = $(this.graph).offset(),
-            x = rawPosition.x - graphPosition.left,
-            y = rawPosition.y - graphPosition.top,
-            position = {x:x, y:y},
-            contents = this.computeWidth(imageDescriptor.name),
-            label = contents[1];
-
-
-
-        this.graphInstance.add([{
-            group: 'nodes',
-            renderedPosition: position,
-            data: {
-                id: imageDescriptor.id,
-                resizable:false,
-                label: label,
-                icon: `/hasli/api/v1/storage/s3/images/${imageDescriptor.logo_url.large}`
-            },
-        }])
-    }
-
-
-    computeWidth(value:string) : [number, string] {
-        let len = value.length,
-            substring = value.substring(0, 10);
-        return [140, substring]
-    }
-
-
-    toggleRight() {
-        this.rightVisible = this.toggle(
-            this.rightSidebar,
-            this.rightVisible,
-            'left'
-        );
-
-    }
-
-    private getCenterWidth(count: number): void {
-
-
-    }
-
-    private configureSidebars(): void {
-
-    }
-
-    private computeExpanded(visible: boolean) {
-        return visible ? -1 : 1;
-    }
-
-    private toggle(ele:HTMLElement, v :boolean, direction:string) :boolean {
-
-        let visible = v,
-            expanded = this.computeExpanded(visible),
-            previous = this.states[this.expanded],
-            next = this.states[this.expanded + expanded];
-        $(this.graph).removeClass(previous);
-        console.log("previous", previous);
-        this.prependClass(this.graph, next);
-        console.log("next", next);
-        if(visible) {
-            visible = false;
-            $(ele).hide();
-        } else {
-            visible = true;
-            $(ele).show();
+    attached() : void {
+        if (!mxClient.isBrowserSupported()) {
+            mxUtils.error('Browser is not supported!', 200, false);
         }
-        this.expanded += expanded;
+        else {
+            mxRubberband.defaultOpacity = 1;
+            mxEvent.disableContextMenu(this.container);
+            let graph = new mxGraph(this.container, new mxGraphModel()),
+                select = new mxRubberband(graph),
+                parent = graph.getDefaultParent(),
+                grid = new Grid(graph);
+            graph.gridSize = 40;
+            graph.getModel().beginUpdate();
+            try {
+                var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 100, 100);
+                var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
+                var e1 = graph.insertEdge(parent, null, '', v1, v2);
+            } finally {
+                graph.getModel().endUpdate();
+            }
+            this.grid = grid;
+            this.grid.draw();
+            this.graph = graph;
+            this.configureListeners();
+        }
 
-        this.graphInstance.gridGuide({
-            snapToGrid:true,
-            discreteDrag:true,
-            guidelines:true,
-            panGrid:true
-        });
-        return visible;
     }
 
-    prependClass(sel, strClass) {
-        var el = $(sel);
-        var classes = el.attr('class');
-        classes = strClass + ' ' + classes;
-        el.attr('class', classes);
+    detached() : void {
+        PLATFORM.global.removeEventListener('resize', this.resizeHandler);
+    }
+
+
+    resized() {
+        this.grid.draw();
+    }
+
+    private configureListeners() {
+        PLATFORM.global.addEventListener('resize', this.resizeHandler);
     }
 }
