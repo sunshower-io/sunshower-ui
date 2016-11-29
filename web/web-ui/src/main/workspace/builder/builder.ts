42194d581b3b07630ae4c303696fb114fcb08a4d
Josiah Haswell <josiah.haswell@gmail.com>
2016-11-28 23:16:26 -0700

    Fixing graph

--- web/web-ui/src/main/workspace/builder/builder.ts
+++ web/web-ui/src/main/workspace/builder/builder.ts
@@ -27,37 +27,24 @@ export class Builder {
     grid: Grid;
     container: HTMLElement;
 
+    private leftVisible: boolean = true;
+    private rightVisible: boolean = true;
+    private expanded: number = 2;
+    private states: {[key: number]: string} = {};
+
+
+    private leftSidebar: HTMLElement;
+    private rightSidebar: HTMLElement;
+
     constructor(private client:HttpClient, private taskManager:TaskManager) {
-        console.log("Task manager", taskManager);
+        this.states[2] = "ten";
+        this.states[0] = "sixteen";
+        this.states[1] = "thirteen";
     }
 
     resizeHandler = () => this.resized();
 
     attached() {
-        $('.ui.dropdown')
-            .dropdown();
-
-        this.sidebar = $('.side.menus .ui.sidebar')
-            .sidebar({
-                dimPage: false,
-                transition: 'overlay',
-                exclusive: false,
-                closable: false
-            });
-        $('.menu .grid.layout.icon')
-            .popup({
-                on         : 'click',
-                inline     : true,
-                hoverable  : true,
-                position   : 'bottom center',
-                delay: {
-                    hide: 100
-                }
-            });
-
-
-        this.sidebar
-            .sidebar('toggle');
 
         if (!mxClient.isBrowserSupported()) {
             mxUtils.error('Browser is not supported!', 200, false);
@@ -78,9 +65,6 @@ export class Builder {
 
     detached() : void {
         PLATFORM.global.removeEventListener('resize', this.resizeHandler);
-
-        this.sidebar
-            .sidebar('hide');
     }
 
     resized() {
@@ -130,11 +114,55 @@ export class Builder {
     }
 
 
-    toggle(menu) {
-        console.log("toggling: " + menu);
-        $(`.${menu}--menu`)
-            .sidebar('toggle')
-        ;
+    toggleLeft() {
+        this.leftVisible = this.toggle(
+            this.leftSidebar,
+            this.leftVisible,
+            'right'
+        );
+    };
+
+    toggleRight() {
+        this.rightVisible = this.toggle(
+            this.rightSidebar,
+            this.rightVisible,
+            'left'
+        );
+
     }
+
+    private computeExpanded(visible: boolean) {
+        return visible ? -1 : 1;
+    }
+
+    private toggle(ele:HTMLElement, v :boolean, direction:string) :boolean {
+
+        let visible = v,
+            expanded = this.computeExpanded(visible),
+            previous = this.states[this.expanded],
+            next = this.states[this.expanded + expanded];
+        $(this.graph).removeClass(previous);
+        console.log("previous", previous);
+        this.prependClass(this.graph, next);
+        console.log("next", next);
+        if(visible) {
+            visible = false;
+            $(ele).hide();
+        } else {
+            visible = true;
+            $(ele).show();
+        }
+        this.expanded += expanded;
+
+        return visible;
+    }
+
+    prependClass(sel, strClass) {
+        var el = $(sel);
+        var classes = el.attr('class');
+        classes = strClass + ' ' + classes;
+        el.attr('class', classes);
+    }
+
 
 }
