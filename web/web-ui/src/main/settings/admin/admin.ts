740dfddc3c04475f5b84c229f3a08731a0bc83f7
Lisa Gumerman <lisajgumerman@gmail.com>
2017-01-31 18:02:18 -0700

    tweaking sidenav, adding indexing buttons

--- web/web-ui/src/main/settings/admin/admin.ts
+++ web/web-ui/src/main/settings/admin/admin.ts
@@ -1,6 +1,7 @@
 import {inject} from "aurelia-framework";
 import {HttpClient} from "aurelia-fetch-client";
 import {User} from "../../../model/core/security";
+import {bindable} from "aurelia-framework";
 
 
 @inject(HttpClient)
@@ -8,10 +9,37 @@ export class Admin {
 
     private pendingSignups:RegistrationRequest[];
 
+    @bindable
+    private indexingImages:boolean;
+
+    @bindable
+    private indexingAWS:boolean;
+
+
     constructor(private client: HttpClient) {
 
     }
 
+    indexImages() : void {
+        this.indexingImages = true;
+        this.client.fetch('docker/images/index', {
+             method: 'get'
+         }).then(response => response.json() as any)
+             .then(r =>
+                 //this.indexingImages = false
+                 console.log('booped the images')
+             );
+    }
+
+    indexAWS() : void {
+         this.indexingAWS = true;
+         this.client.fetch('storage/s3/images', {
+             method: 'get'
+         }).then(response => response.json() as any)
+             .then(r =>
+                 this.indexingAWS = false
+             );
+    }
 
     public activateUser(id:string) : void {
         this.client.fetch(`signup/${id}/approve`, {
@@ -25,7 +53,7 @@ export class Admin {
             .then(response => response.json() as any)
             .then(r => {
                 this.pendingSignups = r;
-                console.log(this.pendingSignups[0].requestId);
+                //console.log(this.pendingSignups[0].requestId);
             });
     }
 
