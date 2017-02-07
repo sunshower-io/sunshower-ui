f8ad5caf4c70f46289dc03b9628dc54723bfff4e
Dustin Lish <dlish27@gmail.com>
2017-02-07 14:30:54 -0700

    moving overview widgets to workspaces

--- web/web-ui/src/main/workspaces/applications/applications.ts
+++ web/web-ui/src/main/workspaces/applications/applications.ts
@@ -1,7 +1,32 @@
-/**
- * Created by dustinlish on 2/6/17.
- */
+
+import {bindable} from "aurelia-framework";
 
 export class Applications {
 
+    @bindable
+    applications: Application[];
+
+    @bindable
+    loading: boolean;
+
+    attached(): void {
+        this.applications = [''];
+    };
+
+    refresh(): void {
+        this.loading = true;
+    }
+
+}
+
+//leaving this here so Josiah can put it wherever
+export class Application {
+    logo    ?: string;
+    name    ?: string;
+    status  ?: string;
+    ip      ?: string;
+    ports   ?: string;
+    cpu     ?: number;
+    memory  ?: number;
+    disk    ?: number;
 }
--- web/web-ui/src/main/workspaces/clouds/clouds.ts
+++ web/web-ui/src/main/workspaces/clouds/clouds.ts
@@ -1,7 +1,33 @@
-/**
- * Created by dustinlish on 2/6/17.
- */
+import {bindable} from "aurelia-framework";
 
 export class Clouds {
 
+    @bindable
+    clouds: Cloud[];
+
+
+    @bindable
+    loading: boolean;
+
+
+    attached(): void {
+        this.clouds = [''];
+    };
+
+    refresh(): void {
+        this.loading = true;
+    }
+
+}
+
+//leaving this here so Josiah can put it wherever
+export class Cloud {
+    logo    ?: string;
+    name    ?: string;
+    status  ?: string;
+    ip      ?: string;
+    ports   ?: string;
+    cpu     ?: number;
+    memory  ?: number;
+    disk    ?: number;
 }
