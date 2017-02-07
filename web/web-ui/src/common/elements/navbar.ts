493c075ec16c3aac62de704ba137d51faed10358
Dustin Lish <dlish27@gmail.com>
2017-02-06 21:06:22 -0700

    fixing some auth/navbar stuff

--- web/web-ui/src/common/elements/navbar.ts
+++ web/web-ui/src/common/elements/navbar.ts
@@ -1,40 +1,8 @@
-import {Banner} from "common/banner/banner";
+
 export class Navbar {
 
-    // @bindable
-    // carouselItems: CarouselViewModel[];
-    //
-    // private banner: Banner;
-
-    private element: Element;
-
-    constructor() {
+    openWorkspaces() {
+        location.assign('#/workspaces');
     }
 
-    attached(): void {
-        let
-            getDistance = () => {
-                let topDist = h.offsetTop + 700;
-                return topDist;
-            },
-            h = this.element as HTMLElement,
-            stuck = false,
-            stickPoint = getDistance();
-
-        // window.onscroll = (e) => {
-        //     let distance = getDistance() - window.pageYOffset,
-        //         offset = window.pageYOffset;
-        //     if ((distance <= 0) && !stuck) {
-        //         h.style.position = 'fixed';
-        //         h.style.top = '0px';
-        //         this.banner.close();
-        //         stuck = true;
-        //     } else if (stuck && (offset <= stickPoint)) {
-        //         h.style.position = 'static';
-        //         this.banner.open();
-        //         stuck = false;
-        //     }
-        // }
-
-    }
 }
