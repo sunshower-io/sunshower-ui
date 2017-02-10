f5397cc96166d5a869b27aa9037abf8f6656a714
Josiah Haswell <josiah.haswell@gmail.com>
2017-02-09 19:28:02 -0700

    adding workspace initial view back in

--- web/web-ui/src/apps/workspaces/index.ts
+++ web/web-ui/src/apps/workspaces/index.ts
@@ -1,3 +1,21 @@
+import {Router} from "aurelia-router";
+import {RouterConfiguration} from "aurelia-router";
+
+
 export class Workspace {
 
+    private router: Router;
+
+    public configureRouter(config: RouterConfiguration, router: Router) {
+        config.title = 'Workspaces';
+        this.router = router;
+        config.title = '';
+        config.map([{
+            route: ['', 'home'],
+            name: 'home',
+            moduleId: './routes/workspace/index',
+            nav: false,
+            title: 'Home'
+        }]);
+    }
 }
