eee20b8b7df0beb2287ea87f895eb5455b76a7f9
Lisa Gumerman <lisajgumerman@gmail.com>
2017-03-01 12:37:26 -0700

    style and drag-dropifying of new workspace

--- web/web-ui/src/apps/workspaces/routes/workspace/create/create.ts
+++ web/web-ui/src/apps/workspaces/routes/workspace/create/create.ts
@@ -1,21 +1,43 @@
-import {autoinject} from 'aurelia-framework';
+import {inject, bindable, NewInstance} from 'aurelia-framework';
 import {Workspace} from "apps/workspaces/routes/workspace/index";
 import {HttpClient} from "aurelia-http-client";
+import {
+    ValidationController,
+    ValidationRules
+} from 'aurelia-validation';
+import {BootstrapFormRenderer} from 'common/resources/custom-components/bootstrap-form-renderer';
 import {UUID} from "common/lib/utils/uuid";
 
-@autoinject
+@inject(Workspace, HttpClient, NewInstance.of(ValidationController))
 export class Create {
 
-    private imageElement: HTMLInputElement;
+    private uploader: HTMLElement;
 
     private name:string;
     private loading:boolean;
     private description:string;
+    private imageElement:HTMLInputElement;
+
+    @bindable
+    private files: FileList;
 
 
-    constructor(private parent: Workspace, private client: HttpClient) {
+    constructor(private parent: Workspace, private client: HttpClient, private controller:ValidationController) {
+        this.controller.addRenderer(new BootstrapFormRenderer());
+    }
 
-
+    attached() : void {
+        this.setupFileUpload();
+        ValidationRules
+            .ensure((wsp:Create) => wsp.name).required()
+            .ensure((wsp:Create) => wsp.description).required()
+            .on(Create);
+        // let imageValidator = ValidationRules
+        //     .ensureObject()
+        //         .satisfies((files:FileList) => files.length > 0)
+        //         .withMessage('An image to represent the workspace is required')
+        //     .rules;
+        // this.controller.addObject(this.files, imageValidator);
     }
 
     activate(): void {
@@ -24,25 +46,65 @@ export class Create {
 
 
     create(): void {
-        this.loading = true;
-        let form = new FormData();
-        let fd = form as any;
+        this.controller.validate().then(result => {
+            if (result.valid) {
+                this.loading = true;
+                let form = new FormData();
+                let fd = form as any;
 
-        form.append('name', this.name);
-        fd.append('image', this.imageElement.files[0]);
-        form.append('image-name', this.imageElement.files[0].name);
-        form.append('description', this.description);
-        form.append('image-type', this.imageElement.files[0].type);
-        this.client.post('workspaces/save', form)
-            .then(t => {
-                this.loading = false;
-                this.parent.router.navigate('/');
-            });
+                form.append('name', this.name);
+                fd.append('image', this.files[0]);
+                form.append('image-name', this.files[0].name);
+                form.append('description', this.description);
+                form.append('image-type', this.files[0].type);
+                this.client.post('workspaces/save', form)
+                    .then(t => {
+                        this.loading = false;
+                        this.parent.router.navigate('/');
+                        //todo redirect to this workspace?
+                    });
+            }
+        });
     }
 
     onchange(): void {
+        this.files = this.imageElement.files;
+    }
 
-        console.log(this.imageElement.files[0].type);
+    setupFileUpload() : void {
+        let $form = $(this.uploader),
+            $input    = $form.find('input[type="file"]'),
+            $label    = $form.find('.upload-box__file-label'),
+            showFiles = function(files) {
+                $label.text(files[0].name);
+            },
+            isAdvancedUpload = function() {
+                let div = document.createElement('div');
+                return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+            }();
+        if (isAdvancedUpload) {
+            $form.addClass('has-advanced-upload');
+
+            $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+            })
+                .on('dragover dragenter', function() {
+                    $form.addClass('is-dragover');
+                })
+                .on('dragleave dragend drop', function() {
+                    $form.removeClass('is-dragover');
+                })
+                .on('drop', (e) => {
+                    this.files = (e.originalEvent as DragEvent).dataTransfer.files;
+                    showFiles( this.files );
+                    console.log(this.files && this.files.length > 0)
+                });
+        }
+
+        $input.on('change', function(e) {
+            showFiles((e as any).target.files);
+        });
     }
 
 
