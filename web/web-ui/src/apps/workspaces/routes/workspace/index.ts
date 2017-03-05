9d75b64cf4bd6db7a48eaeb499fcf53292703062
Dustin Lish <dlish27@gmail.com>
2017-03-05 11:52:08 -0700

    adding environemnt route

--- web/web-ui/src/apps/workspaces/routes/workspace/index.ts
+++ web/web-ui/src/apps/workspaces/routes/workspace/index.ts
@@ -56,97 +56,25 @@ export class Workspace {
         config.map([
 
             // Dashboard
-            {
-                route: ['', 'dashboard'],
-                name: 'dashboard',
-                moduleId: './dashboard/dashboard',
-                nav: true,
-                title: 'Dashboard'
-            },
+            {route: ['', 'dashboard'], name: 'dashboard', moduleId: './dashboard/dashboard', nav: true, title: 'Dashboard'},
 
-            {
-                route: 'create',
-                name: 'create',
-                moduleId: './create/create',
-                nav: false,
-                title: 'Create'
-            },
             // Application Routes
-            {
-                route: 'applications',
-                name: 'applications',
-                moduleId: './applications/applications',
-                nav: true,
-                title: 'Applications'
-            },
-            {
-                route: 'applications/:id/application',
-                name: 'application',
-                moduleId: './applications/application/application',
-                nav: false,
-                title: 'Application'
-            },
-            {
-                route: 'applications/new',
-                name: 'add-application',
-                moduleId: './applications/add-application',
-                nav: false,
-                title: 'Applications'
-            },
+            {route: 'applications',                 name: 'applications',    moduleId: './applications/applications',            nav: true,  title: 'Applications'},
+            {route: 'applications/:id/application', name: 'application',     moduleId: './applications/application/application', nav: false, title: 'Application'},
+            {route: 'applications/new',             name: 'add-application', moduleId: './applications/add-application',         nav: false, title: 'Applications'},
 
             // Infrastructure Routes
-            {
-                route: 'infrastructure',
-                name: 'infrastructure',
-                moduleId: './infrastructure/infrastructure',
-                nav: true,
-                title: 'Infrastructure'
-            },
-            {route: 'clouds', name: 'clouds', moduleId: './infrastructure/clouds/clouds', nav: false, title: 'Clouds'},
-            {
-                route: 'clouds/new',
-                name: 'create-cloud',
-                moduleId: './infrastructure/clouds/add-cloud',
-                nav: false,
-                title: 'Clouds'
-            },
-            {
-                route: 'clouds/:id/credential/new',
-                name: 'add-cloud-credential',
-                moduleId: './infrastructure/clouds/add-credential',
-                nav: false,
-                title: 'Add Cloud Credential'
-            },
+            {route: 'infrastructure',            name: 'infrastructure',       moduleId: './infrastructure/infrastructure',        nav: true,  title: 'Infrastructure'},
+            {route: 'clouds',                    name: 'clouds',               moduleId: './infrastructure/clouds/clouds',         nav: false, title: 'Clouds'},
+            {route: 'clouds/new',                name: 'create-cloud',         moduleId: './infrastructure/clouds/add-cloud',      nav: false, title: 'Clouds'},
+            {route: 'clouds/:id/credential/new', name: 'add-cloud-credential', moduleId: './infrastructure/clouds/add-credential', nav: false, title: 'Add Cloud Credential'},
+            {route: 'environment',               name: 'environment',          moduleId: './infrastructure/clouds/environment/environment', nav: false, title: 'Environment'},
 
             // Provisioning Routes
-            {
-                route: 'provisioning',
-                name: 'provisioning',
-                moduleId: './provisioning/provisioning',
-                nav: true,
-                title: 'Provisioning'
-            },
-            {
-                route: 'instances',
-                name: 'instances',
-                moduleId: './provisioning/instances/instances',
-                nav: false,
-                title: 'Instances'
-            },
-            {
-                route: 'instances/new',
-                name: 'new-instance',
-                moduleId: './provisioning/instances/new',
-                nav: false,
-                title: 'New Instance'
-            },
-            {
-                route: 'instances/:id/instance',
-                name: 'instance',
-                moduleId: './provisioning/instances/instance/instance',
-                nav: false,
-                title: 'Instance'
-            },
+            {route: 'provisioning',           name: 'provisioning', moduleId: './provisioning/provisioning',                nav: true, title: 'Provisioning'},
+            {route: 'instances',              name: 'instances',    moduleId: './provisioning/instances/instances',         nav: false, title: 'Instances'},
+            {route: 'instances/new',          name: 'new-instance', moduleId: './provisioning/instances/new',               nav: false, title: 'New Instance'},
+            {route: 'instances/:id/instance', name: 'instance',     moduleId: './provisioning/instances/instance/instance', nav: false, title: 'Instance'},
 
             // Designer
             {route: 'designer', name: 'designer', moduleId: './designer/designer', nav: false, title: 'Designer'},
@@ -154,6 +82,8 @@ export class Workspace {
             // Settings
             {route: 'settings', name: 'settings', moduleId: './settings/settings', nav: true, title: 'Settings'},
 
+            // Workspace
+            {route: 'create', name: 'create', moduleId: './create/create', nav: false, title: 'Create'},
 
         ]);
 
