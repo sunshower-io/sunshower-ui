8b473084844a4d83b99c8062d43d26442122cba7
Josiah Haswell <josiah.haswell@gmail.com>
2017-03-15 00:57:30 -0600

    Adding whatwg-fetch back into typings

--- web-ui/typings/index.d.ts
+++ web-ui/typings/index.d.ts
@@ -12,6 +12,7 @@
 /// <reference path="globals/showdown/index.d.ts" />
 /// <reference path="globals/url-search-params-typing/index.d.ts" />
 /// <reference path="globals/velocity-animate/index.d.ts" />
+/// <reference path="globals/whatwg-fetch/index.d.ts" />
 /// <reference path="globals/whatwg-streams/index.d.ts" />
 /// <reference path="modules/aurelia-animator-velocity/index.d.ts" />
 /// <reference path="modules/aurelia-binding/index.d.ts" />
