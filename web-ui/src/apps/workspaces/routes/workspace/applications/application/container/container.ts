6bfc7967e0cd730f77e29420fab9e97f0dddb3ac
Dustin Lish <dlish27@gmail.com>
2017-03-26 14:25:54 -0600

    updating routes to match mockups

--- web-ui/src/apps/workspaces/routes/workspace/applications/application/container/container.ts
+++ web-ui/src/apps/workspaces/routes/workspace/applications/application/container/container.ts
@@ -16,18 +16,10 @@ export class Container {
 
     public configureRouter(config: RouterConfiguration, router: Router) {
         config.map([
-            {route: ['', 'general'], name: 'General', moduleId: './settings/general', nav: true, title: 'General'},
-            {route: 'ports', name: 'Ports', moduleId: './settings/ports', nav: true, title: 'Ports'},
-            {route: 'volumes', name: 'Volumes', moduleId: './settings/volumes', nav: true, title: 'Volumes'},
+            // {route: ['', 'general'], name: 'General', moduleId: './settings/general', nav: true, title: 'General'},
+            {route: ['', 'dockerfile'], name: 'Dockerfile', moduleId: './settings/dockerfile', nav: true, title: 'Dockerfile'},
+            {route: 'variables', name: 'Variables', moduleId: './settings/variables', nav: true, title: 'Variables'},
             {route: 'files', name: 'Files', moduleId: './settings/files', nav: true, title: 'Files'},
-            {
-                route: 'dockerfile',
-                name: 'Dockerfile',
-                moduleId: './settings/dockerfile',
-                nav: true,
-                title: 'Dockerfile'
-            },
-            {route: 'args', name: 'Args', moduleId: './settings/args', nav: true, title: 'Args'},
             {route: 'output', name: 'output', moduleId: './settings/output', nav: true, title: 'Output'},
         ]);
 
