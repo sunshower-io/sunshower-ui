89722ddbc0b68964fa4cdb8a02b88562166db7d1
Dustin Lish <dlish27@gmail.com>
2017-03-25 21:23:58 -0600

    removing terminaljs for xterm.js, intial implementation

--- web-ui/src/apps/workspaces/routes/workspace/applications/application/container/settings/output.ts
+++ web-ui/src/apps/workspaces/routes/workspace/applications/application/container/settings/output.ts
@@ -1,5 +1,9 @@
 import {autoinject} from "aurelia-framework";
 import {HttpClient} from "aurelia-fetch-client";
+import {bindable} from "aurelia-framework";
+
+import * as Terminal from "xterm"
+
 /**
  * Created by dustinlish on 2/22/17.
  */
@@ -9,16 +13,29 @@ export class Output {
 
     lines:string[];
 
+    @bindable
+    terminal;
+
     constructor(private client:HttpClient) {
 
     }
 
     activate(items:any, p:any, c:any) : void {
+
+    }
+
+    attached() {
+        var term = new Terminal({
+            cursorBlink: true,
+        });
+        term.open(this.terminal);
+
         this.lines = [];
         let ls = docs.split('\n'),
             count = 0,
             interval = setInterval(() => {
                 if(count < ls.length) {
+                    term.writeln(ls[count]);
                     this.lines.push(ls[count]);
                 } else {
                     return clearInterval(interval);
@@ -32,13 +49,11 @@ export class Output {
 
 
 let docs = `
-Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
-Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
-Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
-Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
-Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
-Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
-Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
-Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
-
+ Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
+ Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
+ Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
+ Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
+ Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
+ Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
+ Digest: sha256:39d4b2f8d3f37bc1321b04ca9bca0681c6ba65ef5a8610793383710e3aecf8b5
 `;
