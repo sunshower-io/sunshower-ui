0c2b4d39cbc7bf76a7cb9477187f9ad79a066f65
Dustin Lish <dlish27@gmail.com>
2017-03-28 14:04:19 -0600

    fixing merge issue

--- web-ui/src/apps/workspaces/routes/workspace/applications/application/container/settings/dockerfile.ts
+++ web-ui/src/apps/workspaces/routes/workspace/applications/application/container/settings/dockerfile.ts
@@ -49,33 +49,6 @@ export class Dockerfile {
             });
     }
 
-    private path() : string {
-        return `workspaces/${this.workspaceId}/applications/${this.id}`
-    }
-
-    refresh() : void {
-        this.client.fetch(`${this.path()}/workspace/file`, {
-            method: 'put',
-            body: JSON.stringify({
-                path: 'Dockerfile'
-            })
-        })
-            .then(t => t.json() as any)
-            .then(t => {
-                if(t.children.child && t.children.child.length > 0) {
-                    let child = t.children.child[0];
-                    this.client.fetch(`${this.path()}/workspace/${child.revision}`)
-                        .then(t => t.json())
-                        .then(t => {
-                            let editor = ace.edit('editor');
-                            editor.setTheme('ace/theme/clouds_midnight');
-                            editor.getSession().setMode('ace/mode/yaml');
-                            editor.setValue((t as any).text);
-                        });
-                }
-            });
-    }
-
     attached() : void {
         this.refresh();
     }
