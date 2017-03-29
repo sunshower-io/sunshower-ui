e65ea47b4dc3d339b851ed190b5d8475ebcae815
Lisa Gumerman <lisajgumerman@gmail.com>
2017-03-29 14:31:05 -0600

    removed repository from create app

--- web-ui/src/apps/workspaces/routes/workspace/applications/application/settings.ts
+++ web-ui/src/apps/workspaces/routes/workspace/applications/application/settings.ts
@@ -1,7 +1,46 @@
-/**
- * Created by dustinlish on 2/21/17.
- */
+import {inject} from "aurelia-framework";
+import {Router} from "aurelia-router";
+import {HttpClient} from "aurelia-http-client";
+import {Applications} from "apps/workspaces/routes/workspace/applications/applications";
+import {Application} from "common/model/api/sdk";
 
+
+@inject(HttpClient, Router, Applications)
 export class Settings {
+
+    private workspaceId     : string;
+    private application     : Application;
+    private loading         : boolean;
+
+    constructor(private client: HttpClient,
+                private router: Router,
+                private applications: Applications) {
+
+    }
+
+    saveRepository() : void {
+        this.loading = true;
+        let app = this.application;
+        if (app && app.repository && app.repository.remote && app.repository.remote.location) {
+            if (app.repository.remote.credential.credential == '' && app.repository.remote.credential.secret == '') {
+                app.repository.remote.credential = null;
+            }
+        }
+
+        let form = new FormData();
+        form.append('name', app.name);
+        //TODO Josiah fill this out
+
+        this.client.put(`workspaces/${this.workspaceId}/applications`, form)
+            .then(t => JSON.parse(t.response))
+            .then(t => {
+                this.loading = false;
+            })
+    }
+
+    activate(id: any) {
+        this.workspaceId = id;
+        this.application = new Application();
+    }
 
 }
