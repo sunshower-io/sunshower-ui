6b030e0cba22577ab3e9c552b1b2738f94380720
Lisa Gumerman <lisajgumerman@gmail.com>
2017-04-21 17:41:00 -0600

    reverting infrastructure designer

--- web-ui/src/apps/workspaces/routes/workspace/infrastructure/designer/infrastructure-designer.ts
+++ web-ui/src/apps/workspaces/routes/workspace/infrastructure/designer/infrastructure-designer.ts
@@ -2,8 +2,7 @@
 import {
     autoinject,
     containerless,
-    customElement,
-    bindable
+    customElement
 } from "aurelia-framework";
 import {
     Grid,
@@ -25,42 +24,9 @@ export default class InfrastructureDesigner {
     private canvas              : Canvas;
     private element             : HTMLElement;
 
-    @bindable
-    private panelActive         : boolean;
-
     constructor() {
 
 
-    }
-
-    attached() : void {
-        this.panelActive = true;
-        let designer = new Designer(this.element),
-            canvas = designer.getCanvas();
-
-        // canvas.addGrid(new Grid(canvas, {
-        //     strokeStyle: '#E2E2E2',
-        //     gridSize   : 30,
-        // }));
-
-        canvas.addGrid(new Grid(canvas, {
-            strokeStyle: 'white',
-            gridSize   : 64,
-        }));
-
-
-
-        this.setCanvas(canvas);
-        this.setDesigner(designer);
-    }
-
-
-    public setCanvas(canvas: Canvas) : void {
-        this.canvas = canvas;
-    }
-
-    public setDesigner(designer:Designer) : void {
-        this.designer = designer;
     }
 
 
