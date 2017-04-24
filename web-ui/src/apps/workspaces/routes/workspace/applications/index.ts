96baa13446168189579db4ce543630a95e888432
Lisa Gumerman <lisajgumerman@gmail.com>
2017-04-24 16:15:58 -0600

    adding cards back in

--- web-ui/src/apps/workspaces/routes/workspace/applications/index.ts
+++ web-ui/src/apps/workspaces/routes/workspace/applications/index.ts
@@ -1,3 +1,23 @@
+import {
+    bindable
+} from 'aurelia-framework';
+
 export class WorkspaceApplications {
+
+    @bindable
+    private content: HTMLElement;
+
+    @bindable
+    private panelActive: boolean;
+
+
+    attached() : void {
+
+        if (this.panelActive) {
+            $(this.content).removeClass('body-content-full')
+        } else {
+            $(this.content).addClass('body-content-full')
+        }
+    }
 
 }
