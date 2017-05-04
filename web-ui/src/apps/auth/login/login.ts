d8eaedc72592920740d70561e7f3a066df74c78d
Lisa Gumerman <lisajgumerman@gmail.com>
2017-05-04 15:38:06 -0600

    fixing funky label on login

--- web-ui/src/apps/auth/login/login.ts
+++ web-ui/src/apps/auth/login/login.ts
@@ -52,7 +52,7 @@ export class Login {
     }
 
     attached(): void {
-        // Materialize.updateTextFields();
+        Materialize.updateTextFields();
         let token = this.storage.get("X-AUTH-TOKEN");
         if (token) {
             this.client.fetch('authenticate/validate', {
