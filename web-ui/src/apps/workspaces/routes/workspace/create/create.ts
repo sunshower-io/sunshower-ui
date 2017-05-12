8c0861acab7339c5808043b3b8adca0154de0c3a
Lisa Gumerman <lisajgumerman@gmail.com>
2017-05-12 12:56:36 -0600

    working on tabs, cards (haven't seen any of it, probably looks terribad)

--- web-ui/src/apps/workspaces/routes/workspace/create/create.ts
+++ web-ui/src/apps/workspaces/routes/workspace/create/create.ts
@@ -35,11 +35,9 @@ export class CreateWorkspace {
         // this.workspace.bindFiles(this.files);
         this.workspaceService.save(this.workspace)
             .catch(err => {
-                //todo get proper errors
-                this.constraintViolation = err;
-                console.log(err);
+                // this.constraintViolation = err;
                 this.showError = true;
-                this.error = 'Oops, something is amiss.';
+                this.error = err.statusText;
             })
             .then(result => {
                 if(result) {
--- web-ui/src/apps/workspaces/routes/workspace/dashboard/index.ts
+++ web-ui/src/apps/workspaces/routes/workspace/dashboard/index.ts
@@ -1,3 +1,14 @@
+import {WorkspaceService} from "apps/workspaces/lib/model/core/workspace";
+
 export class WorkspaceDashboard {
 
+    private workspaceId: string;
+    private instances: any[];
+
+    constructor(private workspaceService:WorkspaceService) {
+        this.workspaceId = this.workspaceService.workspace.id
+        this.instances = [];
+        this.instances.push({name: 'Fake Instance', state: 'Released', nodes: '2', containers: '7', time: '7 days', version: '1.0.23'});
+        this.instances.push({name: 'Another Fake Instance', state: 'Stopped', nodes: '2', containers: '7', time: '5 minutes', version: '1.0.23'});
+    }
 }
