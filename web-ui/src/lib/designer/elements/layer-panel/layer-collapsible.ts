999fe2e8c640f40fd09c5adea18023897ccd6253
Lisa Gumerman <lisajgumerman@gmail.com>
2017-06-05 13:15:41 -0600

    initial redo of layer panel

---
+++ web-ui/src/lib/designer/elements/layer-panel/layer-collapsible.ts
@@ -0,0 +1,29 @@
+import {LayerNode} from "./layer-panel";
+import {bindable, containerless, customElement} from "aurelia-framework";
+import {createEvent} from "lib/common/dom";
+
+@customElement('layer-collapsible')
+@containerless
+export class LayerCollapsible {
+
+    @bindable
+    private nodes : LayerNode[];
+
+    @bindable
+    private holder : HTMLElement;
+
+    constructor() {
+
+    }
+
+    attached() {
+        $(this.holder).find('.collapsible').collapsible();
+    }
+
+    select(node: LayerNode, event: Event) {
+        let target = event.target;
+        //todo create event with createEvent
+    }
+
+
+}
--- web-ui/src/lib/designer/elements/layer-panel/layer-panel.ts
+++ web-ui/src/lib/designer/elements/layer-panel/layer-panel.ts
@@ -1,4 +1,6 @@
 import {containerless, customElement} from "aurelia-framework";
+import {DesignerManager} from "lib/designer/core/designer-manager";
+import {Layer} from "mxgraph";
 
 @containerless
 @customElement('layer-panel')
@@ -6,7 +8,54 @@ export class LayerPanel {
 
     layerPanelCollapsible: HTMLElement;
 
-    attached() {
-        $(this.layerPanelCollapsible).find('.collapsible').collapsible();
+    layers: LayerNode[];
+
+    constructor(private readonly designerManager: DesignerManager) {
+
     }
+
+    attached() {
+        this.layers = [];
+
+        this.designerManager.getCurrentCanvas().listen('graph-changed').forEach(t => {
+            this.layers = [];
+            let outerArray = (t.data as any);
+            for (let i = 0; i < outerArray.length; i++) {
+                let node = this.parseNode(outerArray[i]);
+                if (node != null) {
+                    this.layers.push(node);
+                }
+            }
+        });
+    }
+
+    parseNode(node: Layer) : LayerNode {
+        if (node && node.isVertex()) {
+            let layerNode = new LayerNode();
+            layerNode.title = (node as any).label;
+            layerNode.icon = (node as any).displayIcon;
+            layerNode.self = node;
+
+            if (node.children) {
+                for (let j = 0; j < node.children.length; j++) {
+                    let childNode = this.parseNode(node.children[j]);
+                    if (childNode != null) {
+                        layerNode.children.push(childNode);
+                    }
+                }
+            }
+
+            return layerNode;
+        }
+        return null;
+    }
+
+}
+
+
+export class LayerNode {
+    title:          string;
+    icon:           string;
+    self:           any;
+    children:       LayerNode[] = [];
 }
